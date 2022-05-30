819bb70939883574dea79e1ba173c3dd42fece15
jackluson <18219112108@163.com>
2022-05-30 10:24:58 +0800

    chore: update new solutions

--- solutions/14.new.js
+++ solutions/14.new.js
@@ -1,17 +1,23 @@
 function _new(fn, ...args) {
-  console.log("args", args);
-  const obj = Object.create(Object.prototype); // Object.setPrototypeof(obj, Object.prototype)
+  const obj = Object.create(fn.prototype); // Object.setPrototypeof(obj, Object.prototype)
+  // Object.setPrototypeOf(obj, fn.prototype);
   const res = fn.apply(obj, args);
-  console.log("res", res);
   return res instanceof Object ? res : obj;
 }
 
 function Person(name, age) {
-  console.log("name", name);
-  console.log("age", age);
+  console.log('name', name);
+  console.log('age', age);
   this.name = name;
   this.age = age;
 }
 
-const obj = _new(Person, "jack", 18);
-console.log("obj", obj);
+Person.prototype.getName = function () {
+  return this.name;
+};
+
+const obj = _new(Person, 'jack', 18);
+// const obj1 = new Person('jack', 18);
+console.log('obj', obj);
+console.log('name', obj.getName());
+// console.log('name', obj1, obj1.getName());
